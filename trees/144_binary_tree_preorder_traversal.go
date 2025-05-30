5b52a4e1e6b9dba1a479f82f1b59d87407e5b0b2
Leong Zhe Cheng <zhecheng555@gmail.com>
2025-05-31 00:09:28 +0800

    31/05/2025 Binary Tree Preorder Traversal

---
+++ trees/144_binary_tree_preorder_traversal.go
@@ -0,0 +1,20 @@
+package trees
+
+func PreorderTraversal(root *TreeNode) []int {
+	values := make([]int, 0)
+
+	var dfs func(root *TreeNode) 
+	dfs = func(root *TreeNode) {
+		if root == nil {
+			return
+		}
+
+		values = append(values, root.Val)
+		dfs(root.Left)
+		dfs(root.Right)
+	}
+
+	dfs(root)
+
+	return values
+}
