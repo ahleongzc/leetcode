4310db780c1a28c83b80dece5704cd105200e0fd
Leong Zhe Cheng <zhecheng555@gmail.com>
2025-05-30 22:05:31 +0800

    30/05/2025 Number of islands - BFS

--- graphs/200_number_of_islands.go
+++ graphs/200_number_of_islands.go
@@ -1,6 +1,51 @@
 package graphs
 
-func NumIslands(grid [][]byte) int {
+func NumIslandBFS(grid [][]byte) int {
+	num := 0
+	num_rows := len(grid)
+	num_cols := len(grid[0])
+	directions := [][]int{{0, 1}, {1, 0}, {0, -1}, {-1, 0}}
+
+	var bfs = func(row, col int) {
+		queue := make([][]int, 0)
+		queue = append(queue, []int{row, col})
+		grid[row][col] = byte('0')
+
+		for len(queue) > 0 {
+			element := queue[0]
+			queue = queue[1:]
+
+			for _, val := range directions {
+				delta_x := val[0]
+				delta_y := val[1]
+
+				new_x := delta_x + element[0]
+				new_y := delta_y + element[1]
+				
+
+				if new_x < 0 || new_x == num_rows || new_y < 0 || new_y == num_cols || grid[new_x][new_y] == byte('0') {
+					continue
+				}
+
+				grid[new_x][new_y] = byte('0')
+				queue = append(queue, []int{new_x, new_y})
+			}
+		}
+	}
+
+	for row := range num_rows {
+		for col := range num_cols {
+			if grid[row][col] == byte('1') {
+				bfs(row, col)
+				num++
+			}
+		}
+	}
+
+	return num
+}
+
+func NumIslandsDFS(grid [][]byte) int {
 	num := 0
 	num_rows := len(grid)
 	num_cols := len(grid[0])
@@ -22,7 +67,7 @@ func NumIslands(grid [][]byte) int {
 		for col := range num_cols {
 			if grid[row][col] == byte('1') {
 				dfs(row, col)
-				num += 1
+				num++
 			}
 		}
 	}
