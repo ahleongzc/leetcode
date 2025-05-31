71561a3d5de1d6d9c7b64805bb1e6061b0cd9845
Leong Zhe Cheng <zhecheng555@gmail.com>
2025-05-31 13:08:09 +0800

    31/05/2025 Meeting Rooms

---
+++ intervals/252_meeting_rooms.go
@@ -0,0 +1,17 @@
+package intervals
+
+import "slices"
+
+func CanAttendMeetings(intervals [][]int) bool {
+	slices.SortFunc(intervals, func(a, b []int) int {
+		return a[0] - b[0]
+	})
+
+	for i := 1; i < len(intervals); i++ {
+		if intervals[i - 1][1] > intervals[i][0] {
+			return false
+		}
+	}
+
+	return true
+}
