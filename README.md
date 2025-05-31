f5d34ed3bd1cdaaeffa6c71157a2f03a8a7ba7d7
Leong Zhe Cheng <zhecheng555@gmail.com>
2025-05-31 18:58:23 +0800

    31/05/2025 Invert Binary Tree

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# Leetcode 
 **Practice Link**: https://neetcode.io/practice?tab=neetcode250
---
+++ trees/226_invert_binary_tree.go
@@ -0,0 +1,15 @@
+package trees
+
+func InvertTree(root *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+
+	left := InvertTree(root.Left)
+	right := InvertTree(root.Right)
+
+	root.Left = right
+	root.Right = left
+
+	return root
+}
