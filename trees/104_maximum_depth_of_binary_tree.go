e67bd814b0e3fd48348703afa7fdfe9d21f064ed
Leong Zhe Cheng <zhecheng555@gmail.com>
2025-06-04 22:46:55 +0800

    04/06/2025 Maximum Depth Of Binary Tree

---
+++ trees/104_maximum_depth_of_binary_tree.go
@@ -0,0 +1,14 @@
+package trees
+
+func MaxDepth(root *TreeNode) int {
+	var dfs func(root *TreeNode) int
+	dfs = func(root *TreeNode) int {
+		if root == nil {
+			return 0
+		}
+
+		return max(1 + dfs(root.Left), 1 + dfs(root.Right))
+	}
+
+	return dfs(root)
+}
