a9ec1ff4efdeea62905d0881014072438b08f2ea
Leong Zhe Cheng <zhecheng555@gmail.com>
2025-06-05 22:22:43 +0800

    05/06/2025 House Robber

---
+++ 1d_dynamic_programming/198_house_robber.go
@@ -0,0 +1,17 @@
+package oneddp
+
+func Rob(nums []int) int {
+	if len(nums) == 1 {
+		return nums[0]
+	}
+
+	dp := make([]int, len(nums))
+	dp[0] = nums[0]
+	dp[1] = max(nums[0], nums[1])
+
+	for i := 2; i < len(nums); i++ {
+		dp[i] = max(nums[i]+dp[i-2], dp[i-1])
+	}
+
+	return dp[len(nums)-1]
+}
--- main.go
+++ main.go
@@ -2,14 +2,14 @@ package main
 
 import (
 	"fmt"
+	oneddp "leetcode/1d_dynamic_programming"
 	_ "leetcode/arrays_and_hashing"
 	_ "leetcode/backtracking"
 	_ "leetcode/linked_list"
 	_ "leetcode/stack"
-	stack "leetcode/stack"
 	_ "leetcode/two_pointers"
 )
 
 func main() {
-	fmt.Println(stack.AsteroidCollision([]int{20, 50, -20, -20}))
+	fmt.Println(oneddp.Rob([]int{2, 7, 9}))
 }
