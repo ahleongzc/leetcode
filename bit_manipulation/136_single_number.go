53756f27977fd0ecad9c1e0f87a8be11ae0079f5
Leong Zhe Cheng <zhecheng555@gmail.com>
2025-06-06 21:58:41 +0800

    06/05/2025 Single Number

---
+++ bit_manipulation/136_single_number.go
@@ -0,0 +1,14 @@
+package bitmanipulation
+
+func SingleNumber(nums[]int) int {
+	if len(nums) == 1 {
+		return nums[0]
+	}
+
+	single := nums[0]
+	for i := 1; i < len(nums); i++ {
+		single ^= nums[i]
+	}
+
+	return single
+}
