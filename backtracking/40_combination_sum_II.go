e7d32ba020e09e2388ff5b0ce61f1403acadcd9f
Leong Zhe Cheng <zhecheng555@gmail.com>
2026-03-19 14:02:23 +0800

    19/03/2026 - combination sum II

--- backtracking/40_combination_sum_II.go
+++ backtracking/40_combination_sum_II.go
@@ -1,39 +1,38 @@
 package backtracking
 
-import "slices"
+import (
+	"slices"
+)
 
 func combinationSum2(candidates []int, target int) [][]int {
-	output := make([][]int, 0)
+	slices.Sort(candidates)
+
+	res := make([][]int, 0)
 	buffer := make([]int, 0)
 
-	candidatesCopy := make([]int, len(candidates))
-	copy(candidatesCopy, candidates)
-	slices.Sort(candidatesCopy)
-
-	var dfs func(index, currSum int)
-	dfs = func(index, currSum int) {
-		if currSum > target {
-			return
-		}
-
-		if currSum == target {
+	var dfs func(index, total int)
+	dfs = func(index, total int) {
+		if total == target {
 			bufferCopy := make([]int, len(buffer))
 			copy(bufferCopy, buffer)
-			output = append(output, bufferCopy)
+			res = append(res, bufferCopy)
 			return
 		}
 
-		for i := index; i < len(candidatesCopy); i++ {
-			// skips duplicate at the same recursion depth
-			if i > index && candidatesCopy[i] == candidatesCopy[i-1] {
+		if index == len(candidates) || total > target {
+			return
+		}
+
+		for i := index; i < len(candidates); i++ {
+			if i > index && candidates[i] == candidates[i-1] {
 				continue
 			}
-			buffer = append(buffer, candidatesCopy[i])
-			dfs(i+1, currSum+candidatesCopy[i])
+			buffer = append(buffer, candidates[i])
+			dfs(i+1, total+candidates[i])
 			buffer = buffer[:len(buffer)-1]
 		}
 	}
 
 	dfs(0, 0)
-	return output
+	return res
 }
