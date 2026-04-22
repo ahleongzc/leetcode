b359f6ccec60ea520d0326ce667b48b850c28116
Leong Zhe Cheng <zhecheng555@gmail.com>
2026-04-22 10:19:34 +0800

    22/04/2026 - binary tree right side view

--- trees/199_binary_tree_right_side_view.go
+++ trees/199_binary_tree_right_side_view.go
@@ -1,37 +1,49 @@
 package trees
 
-func RightSideView(root *TreeNode) []int {
-	if root == nil {
-		return nil
-	}
-
-	view := make([]int, 0)
-
+func rightSideView(root *TreeNode) []int {
 	queue := make([]*TreeNode, 0)
+	res := make([]int, 0)
+	if root == nil {
+		return res
+	}
 	queue = append(queue, root)
 
 	for len(queue) != 0 {
-		for range len(queue) - 1 {
+		last := queue[len(queue)-1]
+		res = append(res, last.Val)
+
+		currQueueLen := len(queue)
+		for range currQueueLen {
 			curr := queue[0]
+			queue = queue[1:]
 			if curr.Left != nil {
 				queue = append(queue, curr.Left)
 			}
 			if curr.Right != nil {
 				queue = append(queue, curr.Right)
 			}
-			queue = queue[1:]
 		}
-
-		last := queue[0]
-		view = append(view, last.Val)
-		if last.Left != nil {
-			queue = append(queue, last.Left)
-		}
-		if last.Right != nil {
-			queue = append(queue, last.Right)
-		}
-		queue = queue[1:]
 	}
 
-	return view
+	return res
+}
+
+func rightSideViewDFS(root *TreeNode) []int {
+	res := make([]int, 0)
+
+	var dfs func(root *TreeNode, depth int)
+	dfs = func(root *TreeNode, depth int) {
+		if root == nil {
+			return
+		}
+		if len(res) == depth {
+			res = append(res, root.Val)
+		}
+
+		dfs(root.Right, depth+1)
+		dfs(root.Left, depth+1)
+	}
+
+	dfs(root, 0)
+	return res
 }
